5951dd2d482c5b7955e224a31021b0ebd22d52aa
bingglebangle <ideaday00@gmail.com>
2024-11-08 17:37:20 +0900

    three

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // BrowserRouter : / 다음 원하는 주소명 사용 가능
 // HashRouter    : /#/ 다음 원하는 주소명 사용 가능
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router
+    <BrowserRouter
       basename={process.env.PUBLIC_URL}
       future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
     >
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/hello" element={<h2>hello</h2>} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
